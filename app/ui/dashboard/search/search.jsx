7ace2e47bdfe50b4c485bc3379458c321f6b6542
02Alexis <m_chael1012@hotmail.com>
2024-02-07 13:30:46 -0500

    search functionality

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,9 +1,27 @@
 "use client";
-
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { MdSearch } from "react-icons/md";
 import styles from "./search.module.css";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = useDebouncedCallback((e) => {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("page", 1);
+
+    if (e.target.value) {
+      e.target.value.length > 2 && params.set("q", e.target.value);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathname}?${params}`);
+  }, 300);
+
   return (
     <div className={styles.container}>
       <MdSearch />
@@ -11,7 +29,7 @@ const Search = ({ placeholder }) => {
         type="text"
         placeholder={placeholder}
         className={styles.input}
-        // onChange={handleSearch}
+        onChange={handleSearch}
       />
     </div>
   );
