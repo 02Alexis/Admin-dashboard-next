879a08f704d96e0a40983682d0dc2ec98953bf70
02Alexis <m_chael1012@hotmail.com>
2024-02-07 12:13:43 -0500

    login page design

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,16 @@
+import styles from "../ui/login/login.module.css";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" name="username" />
+        <input type="password" placeholder="password" name="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
